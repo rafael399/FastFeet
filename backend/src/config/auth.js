710898a74528256a7a80bde9128c356ad48506d0
rafael399 <rafael399@gmail.com>
2020-03-15 12:39:04 -0300

    Autenticação JWT

---
+++ backend/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '3d38600cfeb4ab3284c239aeb5328b37',
+  expiresIn: '15d',
+};
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
 import RecipientController from './app/controllers/RecipientController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
 routes.get('/', (req, res) => res.json({ msg: 'ok' }));
 
 routes.post('/recipients', RecipientController.store);
+routes.post('/sessions', SessionController.store);
 
 export default routes;
